7975b65d948d33d19e7b1adedd03b3c454ea67f8
nikitsch <nikitsch5@gmail.com>
2024-11-19 18:11:00 +0300

    feat: currencies page sorting for alphabeticCode

--- src/app/(tables)/currencies/page.tsx
+++ src/app/(tables)/currencies/page.tsx
@@ -46,45 +46,65 @@ export default function Currencies() {
     }, {});
   };
 
+  const sortCountries = (
+    a: [numericCode: string, item: PageList],
+    b: [numericCode: string, item: PageList],
+  ) => {
+    const nameA = a[1].alphabeticCodes[0];
+    const nameB = b[1].alphabeticCodes[0];
+
+    if (nameA < nameB) {
+      return -1;
+    }
+
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0;
+  };
+
   return (
     <div>
       <h1 className="table-title">Currency</h1>
       {isoCountries?.length && currency ? (
         <div className="list">
-          {Object.entries(getCurrencies(isoCountries)).map(
-            ([numericCode, item]: [numericCode: string, item: PageList]) => {
-              const { _id, alphabeticCodes, countries } = item;
-              const code = +numericCode;
-              const alphabeticCode = alphabeticCodes[0];
-              const checked = getInputCheckboxValue(inactiveCurrencies, code);
+          {Object.entries(getCurrencies(isoCountries))
+            .sort(sortCountries)
+            .map(
+              ([numericCode, item]: [numericCode: string, item: PageList]) => {
+                const { _id, alphabeticCodes, countries } = item;
+                const code = +numericCode;
+                const alphabeticCode = alphabeticCodes[0];
+                const checked = getInputCheckboxValue(inactiveCurrencies, code);
 
-              return (
-                <div
-                  key={_id}
-                  className="grid grid-cols-10 grid-flow-col gap-4 p-2"
-                >
-                  <div className="col-span-1">
-                    <input
-                      id="countries"
-                      type="checkbox"
-                      checked={checked}
-                      onChange={(event) => toggleActive(event.target, code)}
-                    />
-                  </div>
+                return (
                   <div
-                    className={`align-middle col-span-2 ${checked ? 'inactive' : ''}`}
+                    key={_id}
+                    className="grid grid-cols-10 grid-flow-col gap-4 p-2"
                   >
-                    {alphabeticCode}
+                    <div className="col-span-1">
+                      <input
+                        id="countries"
+                        type="checkbox"
+                        checked={checked}
+                        onChange={(event) => toggleActive(event.target, code)}
+                      />
+                    </div>
+                    <div
+                      className={`align-middle col-span-2 ${checked ? 'inactive' : ''}`}
+                    >
+                      {alphabeticCode}
+                    </div>
+                    <div
+                      className={`flex justify-end col-span-7 ${checked ? 'inactive' : ''}`}
+                    >
+                      {countries.join(', ')}
+                    </div>
                   </div>
-                  <div
-                    className={`flex justify-end col-span-7 ${checked ? 'inactive' : ''}`}
-                  >
-                    {countries.join(', ')}
-                  </div>
-                </div>
-              );
-            },
-          )}
+                );
+              },
+            )}
         </div>
       ) : (
         <p>No data :(</p>
